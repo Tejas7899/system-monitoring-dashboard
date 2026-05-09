82dcf1bbb59725b85c47ad8e5116b984c92cf374
Tejas7899 <143998944+Tejas7899@users.noreply.github.com>
2026-05-09 11:55:25 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+import psutil
+
+app = Flask(__name__)
+
+@app.route('/system')
+def system_info():
+    data = {
+        "cpu_percent": psutil.cpu_percent(),
+        "memory_percent": psutil.virtual_memory().percent
+    }
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
